e42c7d0c82cd4a75942537efe4fc21ce7a5d0cfe
tav <tav@espians.com>
2011-12-07 13:35:28 +0000

    app: added a stub package for the rpc implementation.

---
+++ app/togethr/rpc/rpc.go
@@ -0,0 +1,14 @@
+// Public Domain (-) 2011 The Togethr Authors.
+// See the Togethr UNLICENSE file for details.
+
+package rpc
+
+import (
+	"http"
+)
+
+func HandleGet(path string, w http.ResponseWriter, r *http.Request) {
+}
+
+func Handle(path string, w http.ResponseWriter, r *http.Request) {
+}
