eade285ddb1ed4ea547fc02c80010c52dc1a9da8
tav <tav@espians.com>
2012-01-17 00:16:13 +0000

    app: extended item model; added transcoding model.

--- app/togethr/model/item.go
+++ app/togethr/model/item.go
@@ -1,34 +1,56 @@
 // Public Domain (-) 2011-2012 The Togethr Authors.
 // See the Togethr UNLICENSE file for details.
 
-package togethr
-
-import (
-	"json"
-)
-
-const ShardFactor = 2
+package model
 
 type Item struct {
 	// Core structure.
-	About      string           `json:"about"`
-	Aspect     string           `json:"aspect"`
-	By         string           `json:"by"`
-	Created    int64            `json:"created"`
-	From       string           `json:"from"`
-	FromGeo    string           `json:"from_geo"`
-	Hidden     bool             `json:"hidden"`
-	Message    string           `json:"msg"`
-	MessageDOM *json.RawMessage `json:"msg_dom"`
-	Meta       *json.RawMessage `json:"meta"`
-	Parents    []string         `json:"parents"`
-	To         []string         `json:"to"`
-	Value      interface{}      `json:"value"`
-	ValueType  int              `json:"value_type"`
+	About      string      `json:"about"`
+	Aspect     string      `json:"aspect"`
+	By         string      `json:"by"`
+	ByTime     int64       `json:"by_time"`
+	Created    int64       `json:"created"`
+	From       string      `json:"from"`
+	FromGeo    string      `json:"from_geo"`
+	Hidden     bool        `json:"hidden"`
+	Language   string      `json:"lang"`
+	Message    string      `json:"msg"`
+	MessageDOM string      `json:"msg_dom"`
+	Meta       string      `json:"meta"`
+	Parents    []string    `json:"parents"` /* history | reply | via | source */
+	To         []string    `json:"to"`
+	Value      interface{} `json:"value"`
+	ValueType  int         `json:"value_type"`
 	// Backend metadata.
-	version int
+	Index   [][]byte `json:"-"`
+	Status  []string `json:"-"` /* break-top40 */
+	Version int      `json:"-"`
 }
 
-func (item *Item) Version() int {
-	return item.version
+// func (item *Item) Load(c <-chan Property) os.Error {
+// 	return item.version
+// }
+
+// func (item *Item) Save(c chan<- Property) os.Error {
+// 	return item.version
+// }
+
+type ItemContent struct {
+	// Core structure.
+	Content string `json:"content"`
+	Title   string `json:"title"`
+	Type    string `json:"type"`
+	// Backend metadata.
+	Version int
+}
+
+type Transcoding struct {
+	// Core structure.
+	Error   string `json:"error"`
+	Status  int    `json:"status"`
+	Path    string `json:"path"`
+	Type    string `json:"type"`
+	Updated int64  `json:"updated"`
+	// Backend metadata.
+	Version int
 }
